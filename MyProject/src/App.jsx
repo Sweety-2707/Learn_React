58f229a7e522d697ec9e431244462961a27b4a4a
Sweety Sharma <sweety2020727@gmail.com>
2024-05-16 23:42:30 +0530

    react and hooks

---
+++ MyProject/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function onPlus() {
+    if (count < 20) {
+      setCount(count + 1);
+    }
+  }
+
+  function onMinus() {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+
+  return (
+    <>
+      <h1>Time Counter</h1>
+      <h3> Counter Value : {count}</h3>
+      <button id="plus" onClick={onPlus}>Add Value</button>
+      <button id="minus" onClick={onMinus}>Minus Value</button>
+      <footer>Counter limit between 0 and 20 </footer>
+    </>
+  );
+}
+
+export default App;
