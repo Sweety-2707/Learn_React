7907434ab041ebd91b5ab8f8f4f6f599f5b6275e
Sweety Sharma <sweety2020727@gmail.com>
2024-09-28 18:25:08 +0530

    css

--- reduxToDo/src/App.jsx
+++ reduxToDo/src/App.jsx
@@ -7,9 +7,9 @@ function App() {
   const todos = useSelector((state) => state.todos);
 
   return (
-    <div className="bg-[#172842] min-h-screen py-8">
+    <div className="bg-[#05080e] min-h-screen py-8">
       <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-        <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+        <h1 className="text-4xl font-bold text-center mb-8 mt-2  bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text">
           Manage Your Todo's
         </h1>
         <div className="mb-4">
